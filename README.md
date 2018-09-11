428887852c214bf2962354c6e369c1bd542583b0
a348019017 <348019017@qq.com>
2018-09-11 14:07:39 +0800

    Signed-off-by: a348019017 <348019017@qq.com>

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #PongGlobe
 一个实验性.Net跨平台开源GIS Globe，使用了mellinoe/veldrid图形库，目前处于起步阶段。
 
+#Screenshot
+
+![Screenshot1](./images/screenshot.png)
+
--- src/PongGlobe.Windows/EarthApplication.cs
+++ src/PongGlobe.Windows/EarthApplication.cs
@@ -55,7 +55,7 @@ namespace PongGlobe.Windows
             renders.Add(vectorLayerRender);
             var pat2h = @"E:\swyy\Lib\PongGlobe\PongGlobe\assets\Vector\NaturalEarth\110m-populated-places-simple\110m_populated_places_simple.shp";
             var vectorPoint = new PointVectorLayer(pat2h, _scene);
-            renders.Add(vectorPoint);
+            //renders.Add(vectorPoint);
             //var drawline = new DrawLineTool(_scene);
            // renders.Add(drawline);
         }
